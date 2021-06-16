3c00fc1fe798102dd2ed5b7d107f986ba0a1bff8
Oksana Tugusheva <gutuxa@gmail.com>
2021-06-16 17:13:53 +0300

    Initial Commit

---
+++ ContactsSwiftUI/ContactsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsSwiftUIApp.swift
+//  ContactsSwiftUI
+//
+//  Created by Oksana Tugusheva on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
